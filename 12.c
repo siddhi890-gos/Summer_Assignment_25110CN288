a901a97dbcd7369cbf86bdf589467f9d88027411
siddhi890-gos <siddhigoswami890@gmail.com>
2026-06-04 13:35:07 +0530

    Update 12.c

--- 12.c
+++ 12.c
@@ -1,18 +1,19 @@
 #include <stdio.h>
 
 int main() {
-    int a, b, temp;
+    int a, b, gcd, lcm, i;
 
     printf("Enter two numbers: ");
     scanf("%d %d", &a, &b);
 
-    while (b != 0) {
-        temp = b;
-        b = a % b;
-        a = temp;
+    for(i = 1; i <= a && i <= b; i++) {
+        if(a % i == 0 && b % i == 0)
+            gcd = i;
     }
 
-    printf("GCD = %d\n", a);
+    lcm = (a * b) / gcd;
+
+    printf("LCM of %d and %d = %d\n", a, b, lcm);
 
     return 0;
 }
