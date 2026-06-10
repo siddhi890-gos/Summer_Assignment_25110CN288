54f66c251b606214335f57cb77c1bd8d6005c476
siddhi890-gos <siddhigoswami890@gmail.com>
2026-06-10 13:28:30 +0530

    Update 33.cpp

--- week 2/Day 9/33.cpp
+++ week 2/Day 9/33.cpp
@@ -7,8 +7,8 @@ int main() {
     cout << "Enter number of rows: ";
     cin >> n;
 
-    for (int i = n; i >= 1; i--) {
-        for (int j = 1; j <= i; j++) {
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <=n-i+1; j++) {
             cout << "* ";
         }
         cout << endl;
