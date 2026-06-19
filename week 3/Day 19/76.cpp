24efc7006873d017f547e97b035f0a223f3a42ea
SIDDHI GOSWAMI <siddhigoswami890@gmail.com>
2026-06-19 15:02:42 +0530

    Add files via upload

---
+++ week 3/Day 19/76.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+
+    cout << "Enter the order of square matrix: ";
+    cin >> n;
+
+    int matrix[10][10];
+    int diagonalSum = 0;
+
+    cout << "Enter matrix elements:\n";
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            cin >> matrix[i][j];
+        }
+    }
+
+    // Calculate principal diagonal sum
+    for (int i = 0; i < n; i++) {
+        diagonalSum += matrix[i][i];
+    }
+
+    cout << "Sum of principal diagonal elements = "
+         << diagonalSum << endl;
+
+    return 0;
+}
